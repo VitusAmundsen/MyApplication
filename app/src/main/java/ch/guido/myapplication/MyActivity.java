93c04f99c3c0514ea8bc2b9e012784e0860f7589
VitusAmundsen <gmeystre@vtxnet.ch>
2015-07-24 16:54:04 +0200

    Initial commit

---
+++ app/src/main/java/ch/guido/myapplication/MyActivity.java
@@ -0,0 +1,65 @@
+package ch.guido.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MyActivity extends ActionBarActivity {
+
+    public static final String EXTRA_MESSAGE = "ch.guido.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void openSearch() {
+    }
+
+    private void openSettings() {
+    }
+
+
+    /*
+        Method called when user want to send the message
+     */
+    public void sendMessage(View theView) {
+        Intent messageIntent = new Intent(this, DisplayMessageActivity.class);
+        EditText messageText = (EditText) findViewById(R.id.edit_message);
+        String message = messageText.getText().toString();
+        messageIntent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(messageIntent);
+    }
+}
